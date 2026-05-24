2ab35591c620d4207bd1a73f9fae4465efae10cf
Your Name <you@example.com>
2026-05-24 02:06:12 -0700

    Create free-stance-switch.ts

---
+++ datascripts/classes/warrior/free-stance-switch.ts
@@ -0,0 +1,34 @@
+import { std } from "wow/wotlk";
+import { MODULE_NAME } from "../../utils/constants/module-name.constants";
+
+const ID = "FREE_STANCE_SWITCH";
+
+const FREE_STANCE_SWITCH = std.Spells.create(
+  MODULE_NAME,
+  ID + "SPELL",
+  12678,
+).Tags.addUnique(MODULE_NAME, ID + "SPELL_TAG");
+
+FREE_STANCE_SWITCH.Effects.get(0).PointsBase.set(99);
+
+const STANCE_NO_RANGE_ITEM = std.Items.create(MODULE_NAME, ID + "RING")
+  .Class.MISC.set()
+  .InventoryType.FINGER.set()
+  .Name.enGB.set("Free Stance Switch Ring")
+  .Quality.ORANGE.set()
+  .Spells.addMod((spellItem) => spellItem.Spell.set(FREE_STANCE_SWITCH.ID))
+  .DisplayInfo.setSimpleIcon(
+    MODULE_NAME,
+    ID + "RING_ICON",
+    "inv_jewelry_ring_36",
+  );
+
+STANCE_NO_RANGE_ITEM.InlineScripts.OnEquip((_, player) => {
+  const FREE_STANCE_SWITCH_ID = UTAG("nikev", "FREE_STANCE_SWITCH_SPELL_TAG");
+  player.LearnSpell(FREE_STANCE_SWITCH_ID);
+});
+
+STANCE_NO_RANGE_ITEM.InlineScripts.OnUnequip((_, player) => {
+  const FREE_STANCE_SWITCH_ID = UTAG("nikev", "FREE_STANCE_SWITCH_SPELL_TAG");
+  player.RemoveSpell(FREE_STANCE_SWITCH_ID, true, true);
+});
